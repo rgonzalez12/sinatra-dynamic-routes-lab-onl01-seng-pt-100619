4ce82525ce9c25ef7bb489273b486b7f8bf5e0a3
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2020-01-22 04:55:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     answer = ''
     
-    params[:number].times do
+    params[:number].to_i.times do
       answer += params[:phrase]
     end
   end
