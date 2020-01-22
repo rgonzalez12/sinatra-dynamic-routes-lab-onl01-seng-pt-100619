ad53cbaadf94bc390b8f6937a6fdc931378ce74e
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2020-01-22 05:03:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -24,6 +24,11 @@ class App < Sinatra::Base
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
   
+  get '/:operation/:number1/:number2'
+  number1 = params[:number1].to_i
+  number2 = params[:number2].to_i
+  
+  
   
 
 end
